622df5cefc8da00376642a696ce6716a8cc66081
Dinara Dyussembayeva <dinaradyussembayeva2006@gmail.com>
2026-05-18 01:56:15 +0500

    Add DemoToken contract with minting functionality

--- contracts/src/token/DemoToken.sol
+++ contracts/src/token/DemoToken.sol
@@ -1,1 +1,15 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract DemoToken is ERC20 {
+    constructor(address receiver) ERC20("Demo Token", "DEMO") {
+        _mint(receiver, 1_000_000 ether);
+    }
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
